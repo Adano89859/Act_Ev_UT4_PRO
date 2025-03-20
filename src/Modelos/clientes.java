5d651d5c841770eaf92a480a7c7174855c439e6d
Adán Romero <adanromeromarrero@gmail.com>
2025-03-20 01:29:43 +0100

    Guardado con forma de comentar los metodos

--- src/Modelos/clientes.java
+++ src/Modelos/clientes.java
@@ -1,4 +1,11 @@
 package Modelos;
 
 public class clientes {
+
+    /*
+    @parametro clientes
+    @return array con clientes
+    Resultado: Al usar este mnetodo, necesitas un usuario y te devuelve un array de usuarios
+     */
+
 }
