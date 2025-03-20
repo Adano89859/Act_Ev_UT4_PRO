fd6cab8286b3d6d06cfe61624b1532fa7f2b6e38
Adán Romero <adanromeromarrero@gmail.com>
2025-03-20 23:21:39 +0100

    Creación final del método "inicializarPlantasHabitaciones", y su correcta llamada en Main.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,47 +1,40 @@
 package Main;
+import Gestor.Estado;
+import Gestor.Tipo_Habitacion;
 import Modelos.habitaciones;
 
 public class Main {
     public static void main(String[] args) {
 
-        public void inicializarPlantasHabitaciones(){
-
-            //Array[][]Hotel = new Array[3];
-
-        }
-
-        public void inicializarPlanta1(){
-            habitaciones[] Planta1 = new habitaciones[5];
-
-            Planta1[0] = new habitaciones();
-            Planta1[1] = new habitaciones();
-            Planta1[2] = new habitaciones();
-            Planta1[3] = new habitaciones();
-            Planta1[4] = new habitaciones();
-
-        }
-
-        public void inicializarPlanta2(){
-            habitaciones[] Planta2 = new habitaciones[5];
-
-            Planta2[0] = new habitaciones();
-            Planta2[1] = new habitaciones();
-            Planta2[2] = new habitaciones();
-            Planta2[3] = new habitaciones();
-            Planta2[4] = new habitaciones();
-
-        }
-
-        public void inicializarPlanta3(){
-            habitaciones[] Planta3 = new habitaciones[5];
-
-            Planta3[0] = new habitaciones();
-            Planta3[1] = new habitaciones();
-            Planta3[2] = new habitaciones();
-            Planta3[3] = new habitaciones();
-            Planta3[4] = new habitaciones();
-
-        }
+        //Recurro a este metodo para crear las habitaciones y un array que las contenga
+        inicializarPlantasHabitaciones();
 
     }
+
+    /*
+    Resultado: Este metodo crea los objetos habitación y los asigna dentro del array Hotel que representa el Hotel
+     */
+    public static void inicializarPlantasHabitaciones(){
+        //Creo el array bidimensional o matriz de Hotel
+        habitaciones[][]Hotel = new habitaciones[3][5];
+        //Introduzco las habitaciones de la planta 1 en el array
+        Hotel[0][0] = new habitaciones(101, Tipo_Habitacion.INDIVIDUAL,10, Estado.DISPONIBLE,"Habitación individual y estandar");
+        Hotel[0][1] = new habitaciones(102, Tipo_Habitacion.INDIVIDUAL,10, Estado.DISPONIBLE,"Habitación individual y estandar");
+        Hotel[0][2] = new habitaciones(103, Tipo_Habitacion.INDIVIDUAL,10, Estado.DISPONIBLE,"Habitación individual y estandar");
+        Hotel[0][3] = new habitaciones(104, Tipo_Habitacion.INDIVIDUAL,10, Estado.DISPONIBLE,"Habitación individual y estandar");
+        Hotel[0][4] = new habitaciones(105, Tipo_Habitacion.INDIVIDUAL,10, Estado.DISPONIBLE,"Habitación individual y estandar");
+        //Introduzco las habitaciones de la planta 2 en el array
+        Hotel[1][0] = new habitaciones(201, Tipo_Habitacion.DOBLE,20, Estado.DISPONIBLE,"Habitación para dos personas, y estandar");
+        Hotel[1][1] = new habitaciones(202, Tipo_Habitacion.DOBLE,20, Estado.DISPONIBLE,"Habitación para dos personas, y estandar");
+        Hotel[1][2] = new habitaciones(203, Tipo_Habitacion.DOBLE,20, Estado.DISPONIBLE,"Habitación para dos personas, y estandar");
+        Hotel[1][3] = new habitaciones(204, Tipo_Habitacion.DOBLE,20, Estado.DISPONIBLE,"Habitación para dos personas, y estandar");
+        Hotel[1][4] = new habitaciones(205, Tipo_Habitacion.DOBLE,20, Estado.DISPONIBLE,"Habitación para dos personas, y estandar");
+        //Introduzco las habitaciones de la planta 3 en el array
+        Hotel[2][0] = new habitaciones(301, Tipo_Habitacion.SUITE,30, Estado.DISPONIBLE,"Habitación suite");
+        Hotel[2][1] = new habitaciones(302, Tipo_Habitacion.SUITE,30, Estado.DISPONIBLE,"Habitación suite");
+        Hotel[2][2] = new habitaciones(303, Tipo_Habitacion.SUITE,30, Estado.DISPONIBLE,"Habitación suite");
+        Hotel[2][3] = new habitaciones(304, Tipo_Habitacion.SUITE,30, Estado.DISPONIBLE,"Habitación suite");
+        Hotel[2][4] = new habitaciones(305, Tipo_Habitacion.SUITE,30, Estado.DISPONIBLE,"Habitación suite");
+    }
+
 }
