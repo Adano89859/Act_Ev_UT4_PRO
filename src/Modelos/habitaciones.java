4896bd15c5d40002ed7f81d5765f37c9626aeecd
Adán Romero <adanromeromarrero@gmail.com>
2025-03-21 10:38:14 +0100

    Los cambios de Habitaciones, sus enum y su implementación en main

--- src/Modelos/habitaciones.java
+++ src/Modelos/habitaciones.java
@@ -15,7 +15,6 @@ public class habitaciones {
     /*
     *CONSTRUCTOR*
     @param Un número de habitación, un tipo de habitación, un precio por noche, un estado, y una descripción.
-    Todo ello traído del main a iniciar la aplicación
     Resultado: Se crea en el Main, introduciendo la información que tendrá este objeto, y desde el constructor atribuyo la info del main al modelo actual
      */
     public habitaciones(int numeroHabitacion,Tipo_Habitacion tipoHabitacion,double precio_noche,Estado estado,String descripcion){
