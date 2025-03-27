b4b24cca6a1dd6f59ec7bcf07b8f4801fa02442b
Adán <adanromeromarrero@gmail.com>
2025-03-27 20:05:35 +0000

    Creado al completo el modelo "reservas".

--- src/Modelos/reservas.java
+++ src/Modelos/reservas.java
@@ -1,4 +1,75 @@
 package Modelos;
 
+import java.time.LocalDateTime;
+
 public class reservas {
+
+    //Atributos de la clase Reserva
+    private String id;
+    private habitaciones habitacion;
+    private clientes cliente;
+    private LocalDateTime check_in;
+    private LocalDateTime check_out;
+    private double precio;
+
+    //CONSTRUCTOR de la clase reserva
+    public reservas(String id,habitaciones habitacion,clientes cliente,LocalDateTime check_in,LocalDateTime check_out,double precio){
+        this.id = id;
+        this.habitacion = habitacion;
+        this.cliente = cliente;
+        this.check_in = check_in;
+        this.check_out = check_out;
+        this.precio = precio;
+    }
+
+    //Los SETTER
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void sethabitacion(habitaciones habitacion){
+        this.habitacion = habitacion;
+    }
+
+    public void setcliente(clientes cliente){
+        this.cliente = cliente;
+    }
+
+    public void setcheck_in(LocalDateTime check_in){
+        this.check_in = check_in;
+    }
+
+    public void setcheck_out(LocalDateTime check_out){
+        this.check_out = check_out;
+    }
+
+    public void setprecio(double precio){
+        this.precio = precio;
+    }
+
+    //Los GETTER
+    public String getId() {
+        return this.id;
+    }
+
+    public habitaciones gethabitacion() {
+        return this.habitacion;
+    }
+
+    public clientes getcliente() {
+        return this.cliente;
+    }
+
+    public LocalDateTime getcheck_in() {
+        return this.check_in;
+    }
+
+    public LocalDateTime getcheck_out() {
+        return this.check_out;
+    }
+
+    public double getprecio() {
+        return this.precio;
+    }
+
 }
